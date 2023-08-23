f3246e07bad364db36f59c34098bec9206f12adc
Athma Vasi <athma.vasi@protonmail.com>
2023-08-22 21:42:11 -0600

    hook up DisplayRepairNotes to DisplayResource, verify fetch

--- src/components/repairNote/constants.ts
+++ src/components/repairNote/constants.ts
@@ -1,3 +1,9 @@
+import { PROVINCES, STATES_US, URGENCY_DATA } from '../../constants/data';
+import { ResourceRoutePaths } from '../../types';
+import { COUNTRIES_DATA } from '../addressChange/constants';
+import { CURRENCY_DATA } from '../benefits/constants';
+import { ComponentQueryData } from '../queryBuilder';
+
 const REQUIRED_REPAIRS_CHECKBOX_DATA = [
   {
     label: 'Cleaning',
@@ -88,4 +94,175 @@ const PARTS_NEEDED_CHECKBOX_DATA = [
   },
 ];
 
-export { PARTS_NEEDED_CHECKBOX_DATA, REQUIRED_REPAIRS_CHECKBOX_DATA };
+const REPAIR_STATUS_DATA = [
+  'In progress',
+  'Waiting for parts',
+  'Awaiting approval',
+  'Completed',
+  'Cancelled',
+];
+
+const REPAIR_NOTE_QUERY_DATA: ComponentQueryData[] = [
+  {
+    label: 'Customer name',
+    value: 'customerName',
+    inputKind: 'textInput',
+  },
+  {
+    label: 'Customer phone',
+    value: 'customerPhone',
+    inputKind: 'textInput',
+  },
+  {
+    label: 'Customer email',
+    value: 'customerEmail',
+    inputKind: 'textInput',
+  },
+  {
+    label: 'Customer address line',
+    value: 'customerAddressLine',
+    inputKind: 'textInput',
+  },
+  {
+    label: 'Customer city',
+    value: 'customerCity',
+    inputKind: 'textInput',
+  },
+  {
+    label: 'Customer state',
+    value: 'customerState',
+    inputKind: 'selectInput',
+    selectData: STATES_US,
+  },
+  {
+    label: 'Customer province',
+    value: 'customerProvince',
+    inputKind: 'selectInput',
+    selectData: PROVINCES,
+  },
+  {
+    label: 'Customer country',
+    value: 'customerCountry',
+    inputKind: 'selectInput',
+    selectData: COUNTRIES_DATA,
+  },
+  {
+    label: 'Customer postal code',
+    value: 'customerPostalCode',
+    inputKind: 'textInput',
+  },
+  // part information
+  {
+    label: 'Part name',
+    value: 'partName',
+    inputKind: 'textInput',
+  },
+  {
+    label: 'Part serial id',
+    value: 'partSerialId',
+    inputKind: 'textInput',
+  },
+  {
+    label: 'Date received',
+    value: 'dateReceived',
+    inputKind: 'dateInput',
+  },
+  {
+    label: 'Description of issue',
+    value: 'descriptionOfIssue',
+    inputKind: 'textInput',
+  },
+  {
+    label: 'Initial inspection notes',
+    value: 'initialInspectionNotes',
+    inputKind: 'textInput',
+  },
+  // repair information
+  {
+    label: 'Required repairs',
+    value: 'requiredRepairs',
+    inputKind: 'selectInput',
+    selectData: REQUIRED_REPAIRS_CHECKBOX_DATA.map(({ label }) => label),
+  },
+  {
+    label: 'Parts needed',
+    value: 'partsNeeded',
+    inputKind: 'selectInput',
+    selectData: PARTS_NEEDED_CHECKBOX_DATA.map(({ label }) => label),
+  },
+  {
+    label: 'Parts needed models',
+    value: 'partsNeededModels',
+    inputKind: 'textInput',
+  },
+  {
+    label: 'Part under warranty',
+    value: 'partUnderWarranty',
+    inputKind: 'booleanInput',
+    booleanData: [true, false],
+  },
+  {
+    label: 'Estimated repair cost',
+    value: 'estimatedRepairCost',
+    inputKind: 'numberInput',
+  },
+  {
+    label: 'Estimated repair cost currency',
+    value: 'estimatedRepairCostCurrency',
+    inputKind: 'selectInput',
+    selectData: CURRENCY_DATA,
+  },
+  {
+    label: 'Estimated completion date',
+    value: 'estimatedCompletionDate',
+    inputKind: 'dateInput',
+  },
+  {
+    label: 'Repair priority',
+    value: 'repairPriority',
+    inputKind: 'selectInput',
+    selectData: URGENCY_DATA,
+  },
+  // ongoing updates
+  {
+    label: 'Repair notes',
+    value: 'repairNotes',
+    inputKind: 'textInput',
+  },
+  {
+    label: 'Testing results',
+    value: 'testingResults',
+    inputKind: 'textInput',
+  },
+  {
+    label: 'Final repair cost',
+    value: 'finalRepairCost',
+    inputKind: 'numberInput',
+  },
+  {
+    label: 'Final repair cost currency',
+    value: 'finalRepairCostCurrency',
+    inputKind: 'selectInput',
+    selectData: CURRENCY_DATA,
+  },
+  {
+    label: 'Repair status',
+    value: 'repairStatus',
+    inputKind: 'selectInput',
+    selectData: REPAIR_STATUS_DATA,
+  },
+];
+
+const REPAIR_NOTE_ROUTE_PATHS: ResourceRoutePaths = {
+  manager: '/api/v1/repair-note/',
+  admin: '/api/v1/repair-note/',
+  employee: '/api/v1/repair-note/user',
+};
+
+export {
+  PARTS_NEEDED_CHECKBOX_DATA,
+  REPAIR_NOTE_QUERY_DATA,
+  REPAIR_NOTE_ROUTE_PATHS,
+  REPAIR_STATUS_DATA,
+  REQUIRED_REPAIRS_CHECKBOX_DATA,
+};
--- src/components/repairNote/display/DisplayRepairNotes.tsx
+++ src/components/repairNote/display/DisplayRepairNotes.tsx
@@ -1,5 +1,14 @@
+import { DisplayResource } from '../../displayResource';
+import { REPAIR_NOTE_QUERY_DATA, REPAIR_NOTE_ROUTE_PATHS } from '../constants';
+
 function DisplayRepairNotes() {
-  return <>REPAIR NOTE</>;
+  return (
+    <DisplayResource
+      componentQueryData={REPAIR_NOTE_QUERY_DATA}
+      paths={REPAIR_NOTE_ROUTE_PATHS}
+      requestBodyHeading="repairNote"
+    />
+  );
 }
 
 export default DisplayRepairNotes;
