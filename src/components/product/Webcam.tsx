533128d38692f97189103c74b85620260775204f
Athma Vasi <athma.vasi@protonmail.com>
2024-06-08 17:31:13 -0600

    add Webcam

---
+++ src/components/product/Webcam.tsx
@@ -0,0 +1,117 @@
+import { Stack } from "@mantine/core";
+
+import { StepperPage } from "../../types";
+import { AccessibleSelectInput } from "../accessibleInputs/AccessibleSelectInput";
+import { ProductCategory } from "../dashboard/types";
+import { CreateProductAction } from "./actions";
+import { AdditionalFields } from "./AdditionalFields";
+import {
+  WEBCAM_FRAME_RATE_DATA,
+  WEBCAM_INTERFACE_DATA,
+  WEBCAM_MICROPHONE_DATA,
+  WEBCAM_RESOLUTION_DATA,
+} from "./constants";
+import { CreateProductDispatch } from "./dispatch";
+import {
+  WebcamFrameRate,
+  WebcamInterface,
+  WebcamMicrophone,
+  WebcamResolution,
+} from "./types";
+
+type WebcamProps = {
+  additionalFields: Array<[string, string]>;
+  additionalFieldsFormData: FormData;
+  parentAction: CreateProductAction;
+  parentDispatch: React.Dispatch<CreateProductDispatch>;
+  productCategory: ProductCategory;
+  stepperPages: StepperPage[];
+  webcamFrameRate: WebcamFrameRate;
+  webcamInterface: WebcamInterface;
+  webcamMicrophone: WebcamMicrophone;
+  webcamResolution: WebcamResolution;
+};
+
+function Webcam({
+  additionalFields,
+  additionalFieldsFormData,
+  parentAction,
+  parentDispatch,
+  productCategory,
+  stepperPages,
+  webcamFrameRate,
+  webcamInterface,
+  webcamMicrophone,
+  webcamResolution,
+}: WebcamProps) {
+  const webcamFrameRateSelectInput = (
+    <AccessibleSelectInput
+      attributes={{
+        data: WEBCAM_FRAME_RATE_DATA,
+        name: "webcamFrameRate",
+        parentDispatch,
+        validValueAction: parentAction.setWebcamFrameRate,
+        value: webcamFrameRate,
+      }}
+    />
+  );
+
+  const webcamInterfaceSelectInput = (
+    <AccessibleSelectInput
+      attributes={{
+        data: WEBCAM_INTERFACE_DATA,
+        name: "webcamInterface",
+        parentDispatch,
+        validValueAction: parentAction.setWebcamInterface,
+        value: webcamInterface,
+      }}
+    />
+  );
+
+  const webcamMicrophoneSelectInput = (
+    <AccessibleSelectInput
+      attributes={{
+        data: WEBCAM_MICROPHONE_DATA,
+        name: "webcamMicrophone",
+        parentDispatch,
+        validValueAction: parentAction.setWebcamMicrophone,
+        value: webcamMicrophone,
+      }}
+    />
+  );
+
+  const webcamResolutionSelectInput = (
+    <AccessibleSelectInput
+      attributes={{
+        data: WEBCAM_RESOLUTION_DATA,
+        name: "webcamResolution",
+        parentDispatch,
+        validValueAction: parentAction.setWebcamResolution,
+        value: webcamResolution,
+      }}
+    />
+  );
+
+  const additionalFieldsAndImage = (
+    <AdditionalFields
+      additionalFields={additionalFields}
+      additionalFieldsFormData={additionalFieldsFormData}
+      parentAction={parentAction}
+      parentDispatch={parentDispatch}
+      productCategory={productCategory}
+      stepperPages={stepperPages}
+    />
+  );
+
+  return (
+    <Stack>
+      {webcamFrameRateSelectInput}
+      {webcamInterfaceSelectInput}
+      {webcamMicrophoneSelectInput}
+      {webcamResolutionSelectInput}
+      {additionalFieldsAndImage}
+    </Stack>
+  );
+}
+
+export { Webcam };
