ce453c44c2897b55e33c2c3b2ceabf5d325ee787
Athma Vasi <athma.vasi@protonmail.com>
2024-06-08 15:53:41 -0600

    add GPU

---
+++ src/components/product/GPU.tsx
@@ -0,0 +1,364 @@
+import { Group, Stack } from "@mantine/core";
+
+import { StepperChild, StepperPage } from "../../types";
+import { AccessibleButton } from "../accessibleInputs/AccessibleButton";
+import { AccessibleSelectInput } from "../accessibleInputs/AccessibleSelectInput";
+import { AccessibleTextAreaInput } from "../accessibleInputs/AccessibleTextAreaInput";
+import { AccessibleImageInput } from "../accessibleInputs/image";
+import { AccessibleTextInput } from "../accessibleInputs/text/AccessibleTextInput";
+import { ProductCategory } from "../dashboard/types";
+import { CreateProductAction } from "./actions";
+import {
+  MAX_ADDITIONAL_FIELDS_AMOUNT,
+  MEMORY_UNIT_DATA,
+  PRODUCT_CATEGORY_PAGE_TABLE,
+} from "./constants";
+import { CreateProductDispatch } from "./dispatch";
+import { MemoryUnit } from "./types";
+
+type GPUProps = {
+  gpuBoostClock: string;
+  gpuChipset: string;
+  gpuCoreClock: string;
+  gpuMemoryCapacity: string;
+  gpuMemoryCapacityUnit: MemoryUnit;
+  gpuTdp: string;
+  additionalFields: Array<[string, string]>;
+  additionalFieldsFormData: FormData;
+  parentAction: CreateProductAction;
+  parentDispatch: React.Dispatch<CreateProductDispatch>;
+  productCategory: ProductCategory;
+  stepperPages: StepperPage[];
+};
+
+function GPU({
+  gpuBoostClock,
+  gpuChipset,
+  gpuCoreClock,
+  gpuMemoryCapacity,
+  gpuMemoryCapacityUnit,
+  gpuTdp,
+  additionalFields,
+  additionalFieldsFormData,
+  parentAction,
+  parentDispatch,
+  productCategory,
+  stepperPages,
+}: GPUProps) {
+  const gpuBoostClockTextInput = (
+    <AccessibleTextInput
+      attributes={{
+        invalidValueAction: parentAction.setPageInError,
+        name: "gpuBoostClock",
+        stepperPages,
+        validValueAction: parentAction.setGpuBoostClock,
+        value: gpuBoostClock,
+      }}
+    />
+  );
+
+  const gpuChipsetTextInput = (
+    <AccessibleTextInput
+      attributes={{
+        invalidValueAction: parentAction.setPageInError,
+        name: "gpuChipset",
+        stepperPages,
+        validValueAction: parentAction.setGpuChipset,
+        value: gpuChipset,
+      }}
+    />
+  );
+
+  const gpuCoreClockTextInput = (
+    <AccessibleTextInput
+      attributes={{
+        invalidValueAction: parentAction.setPageInError,
+        name: "gpuCoreClock",
+        stepperPages,
+        validValueAction: parentAction.setGpuCoreClock,
+        value: gpuCoreClock,
+      }}
+    />
+  );
+
+  const gpuMemoryCapacityTextInput = (
+    <AccessibleTextInput
+      attributes={{
+        invalidValueAction: parentAction.setPageInError,
+        name: "gpuMemoryCapacity",
+        stepperPages,
+        validValueAction: parentAction.setGpuMemoryCapacity,
+        value: gpuMemoryCapacity,
+      }}
+    />
+  );
+
+  const gpuMemoryCapacityUnitSelectInput = (
+    <AccessibleSelectInput
+      attributes={{
+        data: MEMORY_UNIT_DATA,
+        name: "gpuMemoryCapacityUnit",
+        parentDispatch,
+        validValueAction: parentAction.setGpuMemoryCapacityUnit,
+        value: gpuMemoryCapacityUnit,
+      }}
+    />
+  );
+
+  const gpuTdpTextInput = (
+    <AccessibleTextInput
+      attributes={{
+        invalidValueAction: parentAction.setPageInError,
+        name: "gpuTdp",
+        stepperPages,
+        validValueAction: parentAction.setGpuTdp,
+        value: gpuTdp,
+      }}
+    />
+  );
+
+  const addAdditionalFieldButton = (
+    <AccessibleButton
+      attributes={{
+        disabled: additionalFields.length === MAX_ADDITIONAL_FIELDS_AMOUNT,
+        disabledScreenreaderText: "Maximum additional fields amount reached",
+        enabledScreenreaderText: `Add an additional field`,
+        kind: "add",
+        onClick: (
+          _event:
+            | React.MouseEvent<HTMLButtonElement, MouseEvent>
+            | React.PointerEvent<HTMLButtonElement>
+        ) => {
+          parentDispatch({
+            action: parentAction.modifyAdditionalFieldsMap,
+            payload: { operation: "add", productCategory },
+          });
+
+          // required for popover validation linking to stepper component
+          const additionalFieldName: StepperChild = {
+            inputType: "text",
+            name: `Additional Field Name ${additionalFields.length + 1}`,
+            validationKey: "textAreaInput",
+          };
+
+          parentDispatch({
+            action: parentAction.addStepperChild,
+            payload: {
+              dynamicIndexes: [PRODUCT_CATEGORY_PAGE_TABLE[productCategory]],
+              value: additionalFieldName,
+            },
+          });
+
+          const additionalFieldValue: StepperChild = {
+            inputType: "text",
+            name: `Additional Field Value ${additionalFields.length + 1}`,
+            validationKey: "textAreaInput",
+          };
+
+          parentDispatch({
+            action: parentAction.addStepperChild,
+            payload: {
+              dynamicIndexes: [PRODUCT_CATEGORY_PAGE_TABLE[productCategory]],
+              value: additionalFieldValue,
+            },
+          });
+        },
+      }}
+    />
+  );
+
+  const additionalTextInputs = additionalFields.map((tuple, index) => {
+    const [fieldName, fieldValue] = tuple;
+
+    const fieldNameTextAreaInput = (
+      <AccessibleTextAreaInput
+        attributes={{
+          dynamicIndexes: [index],
+          invalidValueAction: parentAction.setPageInError,
+          name: `Additional Field Name ${index + 1}`,
+          productDispatchInfo: {
+            kind: "fieldName",
+            operation: "update",
+            productCategory,
+            productDispatch: parentDispatch,
+          },
+          stepperPages,
+          validValueAction: parentAction.modifyAdditionalFieldsMap,
+          value: fieldName,
+        }}
+      />
+    );
+
+    const fieldValueTextAreaInput = (
+      <AccessibleTextAreaInput
+        attributes={{
+          dynamicIndexes: [index],
+          invalidValueAction: parentAction.setPageInError,
+          name: `Additional Field Value ${index + 1}`,
+          productDispatchInfo: {
+            kind: "fieldValue",
+            operation: "update",
+            productCategory,
+            productDispatch: parentDispatch,
+          },
+          stepperPages,
+          validValueAction: parentAction.modifyAdditionalFieldsMap,
+          value: fieldValue,
+        }}
+      />
+    );
+
+    const deleteFieldButton = (
+      <AccessibleButton
+        attributes={{
+          enabledScreenreaderText: `Delete additional field ${index + 1}`,
+          index,
+          kind: "delete",
+          onClick: (
+            _event:
+              | React.MouseEvent<HTMLButtonElement, MouseEvent>
+              | React.PointerEvent<HTMLButtonElement>
+          ) => {
+            parentDispatch({
+              action: parentAction.modifyAdditionalFieldsMap,
+              payload: {
+                dynamicIndexes: [index],
+                operation: "delete",
+                productCategory,
+              },
+            });
+          },
+          setIconAsLabel: true,
+        }}
+      />
+    );
+
+    const insertFieldButton = (
+      <AccessibleButton
+        attributes={{
+          disabled: additionalFields.length === MAX_ADDITIONAL_FIELDS_AMOUNT,
+          disabledScreenreaderText: "Maximum additional fields amount reached",
+          enabledScreenreaderText: `Insert additional field before ${index + 1}`,
+          index,
+          kind: "insert",
+          setIconAsLabel: true,
+          onClick: (
+            _event:
+              | React.MouseEvent<HTMLButtonElement, MouseEvent>
+              | React.PointerEvent<HTMLButtonElement>
+          ) => {
+            parentDispatch({
+              action: parentAction.modifyAdditionalFieldsMap,
+              payload: {
+                dynamicIndexes: [index],
+                operation: "insert",
+                productCategory,
+              },
+            });
+          },
+        }}
+      />
+    );
+
+    const slideFieldDownButton = (
+      <AccessibleButton
+        attributes={{
+          disabled: index === additionalFields.length - 1,
+          disabledScreenreaderText: `Additional field ${
+            index + 1
+          } is already at the bottom`,
+          enabledScreenreaderText: `Slide additional field down`,
+          index,
+          kind: "down",
+          setIconAsLabel: true,
+          onClick: (
+            _event:
+              | React.MouseEvent<HTMLButtonElement, MouseEvent>
+              | React.PointerEvent<HTMLButtonElement>
+          ) => {
+            parentDispatch({
+              action: parentAction.modifyAdditionalFieldsMap,
+              payload: {
+                dynamicIndexes: [index],
+                operation: "slideDown",
+                productCategory,
+              },
+            });
+          },
+        }}
+      />
+    );
+
+    const slideFieldUpButton = (
+      <AccessibleButton
+        attributes={{
+          disabled: index === 0,
+          disabledScreenreaderText: `Additional field ${index + 1} is already at the top`,
+          enabledScreenreaderText: `Slide additional field up`,
+          index,
+          kind: "up",
+          setIconAsLabel: true,
+          onClick: (
+            _event:
+              | React.MouseEvent<HTMLButtonElement, MouseEvent>
+              | React.PointerEvent<HTMLButtonElement>
+          ) => {
+            parentDispatch({
+              action: parentAction.modifyAdditionalFieldsMap,
+              payload: {
+                dynamicIndexes: [index],
+                operation: "slideUp",
+                productCategory,
+              },
+            });
+          },
+        }}
+      />
+    );
+
+    return (
+      <Stack key={`${productCategory}-${index}`}>
+        <Group>
+          {fieldNameTextAreaInput}
+          {fieldValueTextAreaInput}
+        </Group>
+        <Group>
+          {deleteFieldButton}
+          {insertFieldButton}
+          {slideFieldDownButton}
+          {slideFieldUpButton}
+        </Group>
+      </Stack>
+    );
+  });
+
+  const imageInput = (
+    <AccessibleImageInput
+      attributes={{
+        formData: additionalFieldsFormData,
+        invalidValueAction: parentAction.setPageInError,
+        page: 1,
+        productCategory,
+        productCategoryDispatch: parentDispatch,
+        stepperPages,
+        validValueAction: parentAction.setAdditionalFieldsFormDataMap,
+      }}
+    />
+  );
+
+  return (
+    <Stack>
+      {gpuBoostClockTextInput}
+      {gpuChipsetTextInput}
+      {gpuCoreClockTextInput}
+      {gpuMemoryCapacityTextInput}
+      {gpuMemoryCapacityUnitSelectInput}
+      {gpuTdpTextInput}
+      {addAdditionalFieldButton}
+      {additionalTextInputs}
+      {imageInput}
+    </Stack>
+  );
+}
+
+export { GPU };
